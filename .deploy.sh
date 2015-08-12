97d182eef9647d7e9e7c3e202934db23a0e5cf35
Devon Weller <devon@tokenly.co>
2015-08-12 10:36:14 -0500

    copy environment settings

--- .deploy.sh
+++ .deploy.sh
@@ -5,6 +5,9 @@ set -e
 echo; echo "updating composer dependencies"
 /usr/local/bin/composer.phar install --prefer-dist --no-progress
 
+echo; echo "copying sample environment"
+cp -v .env.example .env
+
 echo; echo "compiling assets"
 DIR=`pwd`
 cd resources/assets;
