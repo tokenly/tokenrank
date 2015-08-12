3c2b4fe99515147168f806ce1b5baa93f388a351
Devon Weller <devon@tokenly.co>
2015-08-12 10:19:07 -0500

    tweak deploy script

--- .deploy.sh
+++ .deploy.sh
@@ -6,4 +6,8 @@ echo; echo "updating composer dependencies"
 /usr/local/bin/composer.phar install --prefer-dist --no-progress
 
 echo; echo "compiling assets"
-$(cd resources/assets && npm install && ./node_modules/gulp/bin/gulp.js)
+DIR=`pwd`
+cd resources/assets;
+npm install;
+./node_modules/gulp/bin/gulp.js
+cd $DIR
